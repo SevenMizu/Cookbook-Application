0c5bd964203e2281f7e816e142e19225bfecb2e6
Samuel <sk224aw@student.lnu.se>
2024-04-08 12:50:24 +0200

    fix: UserScreenController

--- app/src/main/java/cookbook/handlers/UserScreenController.java
+++ app/src/main/java/cookbook/handlers/UserScreenController.java
@@ -1,12 +1,24 @@
 package cookbook.handlers;
 
+import cookbook.DBUtils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class UserScreenController {
 
     @FXML
     private Label activeUserLabel;
+
+
+    @FXML
+    private Button logoutButton;
+
+    @FXML
+    void logout(ActionEvent event) {
+        DBUtils.logout(event);
+    }
 
     public void setActiveUserLabel(String text) {
         activeUserLabel.setText(text);
