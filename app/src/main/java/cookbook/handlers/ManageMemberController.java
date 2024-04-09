601040f6d0affc053def6ed80a9fb522e47b4fa9
bryan <bb222qj@student.lnu.se>
2024-04-09 11:16:00 +0200

    feat: create button and toggling visibilty anchorpane

--- app/src/main/java/cookbook/handlers/ManageMemberController.java
+++ app/src/main/java/cookbook/handlers/ManageMemberController.java
@@ -1,8 +1,12 @@
 package cookbook.handlers;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 public class ManageMemberController {
 
@@ -10,16 +14,34 @@ public class ManageMemberController {
     private Button backButton;
 
     @FXML
+    private AnchorPane createAnchor;
+
+    @FXML
     private Button createButton;
+
+    @FXML
+    private Button databaseCreate;
 
     @FXML
     private Button deleteButton;
 
     @FXML
+    private RadioButton isAdminRadioCreate;
+
+    @FXML
     private ListView<String> memberList;
 
     @FXML
+    private Button discardButton;
+
+    @FXML
     private Button modifyButton;
+
+    @FXML
+    private TextField passwordCreate;
+
+    @FXML
+    private TextField usernameCreate;
 
 
     /**
@@ -28,4 +50,31 @@ public class ManageMemberController {
      */
     public void setMemberList(ObservableList<String> list) {
         memberList.setItems(list);
-    }}
+    }
+
+ 
+    /**
+     * Method to show the createAnchor when createButton is clicked.
+     * This method sets the visibility of the createButton to false and that of the createAnchor to true.
+     * @param event The ActionEvent triggering the method.
+     */
+    @FXML
+    void showCreateAnchor(ActionEvent event) {
+        createButton.setVisible(false);
+        createAnchor.setVisible(true);
+    }
+
+        /**
+     * Method to hide the createAnchor and show the createButton.
+     * This method sets the visibility of the createAnchor to false and that of the createButton to true.
+     * @param event The ActionEvent triggering the method.
+     */
+    @FXML
+    void hideCreateAnchor(ActionEvent event) {
+        createAnchor.setVisible(false);
+        createButton.setVisible(true);
+    }
+
+    
+
+}
