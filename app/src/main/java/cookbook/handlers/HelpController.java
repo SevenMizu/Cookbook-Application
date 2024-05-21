90373b77b7c2b9fed7ed16698e090379625ac4b6
Samuel <sk224aw@student.lnu.se>
2024-05-21 23:21:03 +0200

    feat: help screen

---
+++ app/src/main/java/cookbook/handlers/HelpController.java
@@ -0,0 +1,23 @@
+package cookbook.handlers;
+
+import cookbook.DBUtils;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class HelpController {
+
+    @FXML
+    private Button backToHome;
+
+    /**
+     * Navigates back to the user home screen.
+     * 
+     * @param event The action event triggering the method.
+     */
+    @FXML
+    void backToUserScreen(ActionEvent event) {
+        DBUtils.changeToUserHomeScene("xmls/userHomeScreen.fxml", event, DBUtils.getloggedInuser());
+    }
+
+}
